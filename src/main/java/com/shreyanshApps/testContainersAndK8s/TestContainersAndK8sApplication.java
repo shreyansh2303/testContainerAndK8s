17da89cf0cdfd88f1cf1b599a1d88bdb74347023
Shreyansh Singh <92965081+shreyansh2303@users.noreply.github.com>
2026-05-03 20:34:18 +0530

    Initial commit

---
+++ src/main/java/com/shreyanshApps/testContainersAndK8s/TestContainersAndK8sApplication.java
@@ -0,0 +1,14 @@
+package com.shreyanshApps.testContainersAndK8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class TestContainersAndK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainersAndK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyanshApps/testContainersAndK8s/TestContainersAndK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyanshApps.testContainersAndK8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersAndK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
